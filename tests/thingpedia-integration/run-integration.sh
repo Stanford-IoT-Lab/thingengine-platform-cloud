0b2418997df1869ec4f96866550be296f09d9188
Jim Deng <jim.deng@gmail.com>
2021-07-13 22:23:39 +0000

    ThingpediaIntegrationTest: address comments

--- tests/thingpedia-integration/run-integration.sh
+++ tests/thingpedia-integration/run-integration.sh
@@ -11,5 +11,16 @@ kubectl exec $POD --  bash -c "cd /opt/almond-cloud && npx nyc tests/thingpedia-
 # Run selenium test on ubuntu with Firefox installed
 THINGENGINE_CONFIGDIR=tests/thingpedia-integration/k8s npx nyc ts-node tests/test_website_selenium.js
 
-# Copy coverage outputs from frontend pod
+# kill frontend to generate coverage outputs
+kubectl exec $POD -- bash -c 'kill $(cat /home/almond-cloud/pid)'
+sleep 2
+
+# Copy coverage outputs
 kubectl cp $POD:/opt/almond-cloud/.nyc_output .nyc_output
+
+# kill backend to generate coverage outputs
+kubectl exec shared-backend-0 -- bash -c 'kill $(cat /home/almond-cloud/pid)'
+sleep 2
+
+# Copy coverage outputs
+kubectl cp shared-backend-0:/opt/almond-cloud/.nyc_output .nyc_output
